43b12d76df4852d9905448fb1103983c6094089d
Yael Brown <yaelrbrown@gmail.com>
2017-08-25 02:51:54 -0400

    added toString and playGuitar
    finish playGuitar method. Look up math.random

---
+++ testGuitar.java
@@ -0,0 +1,12 @@
+/*
+ * File: testGuitar.java
+ * Author: Yael Brown
+ * Date: August 24, 2017 
+ * Purpose: To test file, Guitar.java functions
+ */
+
+package com.cmis141.w4.hw2;
+
+/**
+ * Guitar Test
+ */
