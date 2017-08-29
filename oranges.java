2962166b1f603fd12daf469c28dd80bd5b1ff7fa
Yael Brown <yaelrbrown@gmail.com>
2017-08-28 22:08:07 -0400

    l
    l

---
+++ oranges.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class oranges{
+	public static void main(String args[]){
+
+		Random r = new Random();
+
+		r.nextInt();
+
+		System.out.println("Random number is " + r);
+	}
+}
--- playGuitar.java
+++ playGuitar.java
@@ -7,14 +7,21 @@ public class playGuitar
     {
         // Declare variables
         int value;
+        int value2;
         String letter;
+        String noteLength;
         Random r = new Random();
+        Random ra = new Random();
+
+        // Declare int's for notes
+        int pick;
 
         // Declare default values
         letter = null;
 
         // Generate random number
-        value = r.nextInt(7 - 1 + 1) + 1;
+		value = r.nextInt(7 - 1 + 1) + 1;
+	   	value2 = r.nextInt(5 - 1 + 1) + 1;
 
         // Test the value of 'value'
         System.out.println("Value of value is :" + value);
@@ -39,7 +46,37 @@ public class playGuitar
                     break;
         }
 
+        // Switch statement to convert random to noteLength
+        switch (value2) {
+        case 1:     noteLength = ".25";
+                    break;
+        case 2:     noteLength = ".5";
+                    break;
+        case 3:     noteLength = "1";
+                    break;
+        case 4:     noteLength = "2";
+                    break;
+        case 5:     noteLength = "4";
+                    break;
+        default:    noteLength = null;
+                    break;
+        }
+
         // Test output
         System.out.println("Value of letter = " + letter);
+        System.out.println("Value of noteLength = " + noteLength);
+
+        // Beginning output playGuitar into array
+        System.out.print("playGuitar(): { ");
+
+        // Generate
+        for (int i = 0; i < 16; i++) {
+        	System.out.print(letter + "(" + noteLength +") ");
+        	value = new Random(16).nextInt(7 - 1 + 1) + 1;
+	   		value2 = new Random(16).nextInt(5 - 1 + 1) + 1;
+        }
+
+        // Ending output playGuitar into array
+        System.out.print("}");
     }
 }
